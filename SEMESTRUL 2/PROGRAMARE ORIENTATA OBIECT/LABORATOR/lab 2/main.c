f0f919d64fbb1230cf3ad7ad4a95e81a7b51ddba
Costi <costi.mierla284@gmail.com>
2024-03-06 09:15:30 +0200

    lab 2

---
+++ SEMESTRUL 2/PROGRAMARE ORIENTATA OBIECT/LABORATOR/lab 2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * 9. Agentie de turism
+    Creati o aplicatie care permite gestiunea ofertelor de la o agentie de turism.
+    Fiecare oferta are: tip (munte,mare, citiy break), destinatie, data plecare, pret
+    Aplicatia permite:
+     a) Adaugarea de noi oferte.
+     b) Actualizare oferte
+     c) Stergere oferta
+     d) Vizualizare oferete ordonat dupa pret, destinatie (crescator/descrescator)
+     e) Vizualizare oferta filtrate dupa un criteriu (destinatie, tip, pret)
+ */
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
