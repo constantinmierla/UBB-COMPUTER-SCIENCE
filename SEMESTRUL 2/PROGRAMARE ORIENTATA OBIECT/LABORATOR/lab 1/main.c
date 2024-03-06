9c346d2d7b2ebaccb6423eeab74b8a0e3403e436
Costi <costi.mierla284@gmail.com>
2024-03-06 08:42:20 +0200

    working

--- SEMESTRUL 2/PROGRAMARE ORIENTATA OBIECT/LABORATOR/lab 1/main.c
+++ SEMESTRUL 2/PROGRAMARE ORIENTATA OBIECT/LABORATOR/lab 1/main.c
@@ -2,6 +2,9 @@
 
 //9. Citeste un sir de numere naturale nenule terminat cu 0 si determina
 //    numarul cifrelor 0 in care se termina numarul produs al numerelor citite.
+//3. Determina toate reprezentarile posibile a unui numar natural ca suma
+//   de numere naturale consecutive.
+
 int produs_numere(int produs)
 {
     /*
@@ -46,21 +49,87 @@ int cifre_zero(int rezultat)
     }
     return count;
 }
+int suma(int start, int nr)
+{
+    /*
+     * Functia calculeaza suma de numere consecutive incepand cu start
+     * input : start, nr
+     * type input : int
+     * output : 1 (daca exista aceasta suma) sau 0 in caz contrar
+     */
+    int s = 0;
+    while (s < nr)
+    {
+        s = s + start;
+        start++;
+    }
+    if (s == nr)
+        return 1;
+    else
+        return 0;
+}
+void afis(int start, int nr)
+{
+    /*
+     * functia afiseaza suma de numere consecutive
+     * input : start, nr
+     * type input : int
+     * output : sirul de numere consecutive
+     */
+    int s = 0;
+    while (s < nr)
+    {
+        printf("%d ", start);
+        s = s + start;
+        start++;
+    }
+    printf("%s", "\n");
+}
+void solve(int n)
+{
+    /*
+     * functia calculeaza suma de numere consecutive
+     * input : n
+     * type input : int
+     */
+    for (int start = 1; start <= n/2; start++)
+    {
+        if (suma(start, n) == 1)
+            afis(start, n);
+    }
+}
 int main()
 {
-    int ok = 1;
+    int ok = 1, option, nr;
+    printf("%s","Alegeti cerinta : 1(pb 9) sau 2(pb 3) : \n");
+    scanf("%d", &option);
     while(ok == 1)
     {
         int produs = 1, rezultat, cifre;
 
-        rezultat = produs_numere(produs);
+        if (option == 1) {
+            rezultat = produs_numere(produs);
 
-        cifre = cifre_zero(rezultat);
+            cifre = cifre_zero(rezultat);
 
-        printf("%d", cifre);
+            printf("%d", cifre);
+        }
+        else if (option == 2)
+        {
+            printf("%s","Introduceti numarul : ");
+            scanf("%d", &nr);
+            solve(nr);
+        }
+
         printf("%s","\nRulati din nou programul: Da (1), Nu (0) : \n");
+
         scanf("%d", &ok);
 
+        if (ok == 1)
+        {
+            printf("%s", "\nAlegeti optiunea: \n");
+            scanf("%d", &option);
+        }
     }
     printf("%s", "iesire din aplicatie...");
     return 0;
