f8e9685c5f3cc05ddd3ddb1f4197a06e1c1c71b3
Costi <costi.mierla284@gmail.com>
2024-03-14 15:07:29 +0200

    upd

--- SEMESTRUL 2/ALGORITMICA GRAFELOR/LABORATOR/lab 2/ex 5/main.cpp
+++ SEMESTRUL 2/ALGORITMICA GRAFELOR/LABORATOR/lab 2/ex 5/main.cpp
@@ -1,6 +1,35 @@
+/*
+ * 5. Pentru un graf dat să se afișeze pe ecran vârfurile descoperite
+ * de apelul recursiv al procedurii DFS_VISIT(G, u) (apadurea descoperită de DFS).
+ */
 #include <iostream>
+#include <fstream>
+using namespace std;
+ifstream in("graf.txt");
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int viz[102],mat[102][102];
+
+void dfs(int nod, int n)
+{
+    int i;
+    for (i = 1; i <= n; i++)
+    {
+        if(!viz[i] and mat[nod][i])
+        {
+            cout << i << " ";
+            viz[i] = 1;
+            dfs(i,n);
+        }
+    }
+}
+int main()
+{
+    int x,y,n,nod;
+    in >> n;
+    while(in >> x >> y)
+        mat[x][y] = 1;
+    in.close();
+    cin >> nod;
+    dfs(nod, n);
     return 0;
 }
