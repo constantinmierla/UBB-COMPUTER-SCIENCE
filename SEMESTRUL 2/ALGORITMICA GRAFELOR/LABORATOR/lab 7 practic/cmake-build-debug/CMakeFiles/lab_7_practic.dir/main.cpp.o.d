11741ffb01d1353693625e15e4a3ad2d0b2baaed
Costi <costi.mierla284@gmail.com>
2024-06-22 17:57:07 +0300

    upd

--- SEMESTRUL 2/ALGORITMICA GRAFELOR/LABORATOR/lab 7 practic/cmake-build-debug/CMakeFiles/lab_7_practic.dir/main.cpp.o.d
+++ SEMESTRUL 2/ALGORITMICA GRAFELOR/LABORATOR/lab 7 practic/cmake-build-debug/CMakeFiles/lab_7_practic.dir/main.cpp.o.d
@@ -145,12 +145,4 @@ CMakeFiles/lab_7_practic.dir/main.cpp.o: \
  /usr/include/c++/11/bits/locale_facets.tcc \
  /usr/include/c++/11/bits/basic_ios.tcc \
  /usr/include/c++/11/bits/ostream.tcc /usr/include/c++/11/istream \
- /usr/include/c++/11/bits/istream.tcc /usr/include/c++/11/vector \
- /usr/include/c++/11/bits/stl_uninitialized.h \
- /usr/include/c++/11/bits/stl_vector.h \
- /usr/include/c++/11/bits/stl_bvector.h \
- /usr/include/c++/11/bits/vector.tcc /usr/include/c++/11/queue \
- /usr/include/c++/11/deque /usr/include/c++/11/bits/stl_deque.h \
- /usr/include/c++/11/bits/deque.tcc /usr/include/c++/11/bits/stl_heap.h \
- /usr/include/c++/11/bits/stl_queue.h \
- /usr/include/c++/11/bits/uses_allocator.h
+ /usr/include/c++/11/bits/istream.tcc
