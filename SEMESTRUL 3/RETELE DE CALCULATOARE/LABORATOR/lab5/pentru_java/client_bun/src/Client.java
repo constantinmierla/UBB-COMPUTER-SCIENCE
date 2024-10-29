78888125587fae98fa2c635d2955a82296bd50fe
Costi <costi.mierla284@gmail.com>
2024-10-29 22:01:26 +0200

    upd

---
+++ SEMESTRUL 3/RETELE DE CALCULATOARE/LABORATOR/lab5/pentru_java/client_bun/src/Client.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 1234);
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Dimensiunea primului sir: ");
+            int size1 = Integer.parseInt(reader.readLine());
+            out.writeShort(size1);
+
+            for (int i = 0; i < size1; i++) {
+                System.out.print("Introduceți elementele pentru primul sir: ");
+                out.writeShort(Short.parseShort(reader.readLine()));
+            }
+
+            System.out.print("Dimensiunea celui de-al doilea sir: ");
+            int size2 = Integer.parseInt(reader.readLine());
+            out.writeShort(size2);
+
+            for (int i = 0; i < size2; i++) {
+                System.out.print("Introduceți elementele pentru al doilea sir: ");
+                out.writeShort(Short.parseShort(reader.readLine()));
+            }
+
+
+            int resultCount = in.readUnsignedShort();
+
+            for (int i = 0; i < resultCount; i++) {
+                int element = in.readUnsignedShort();
+                System.out.println("Element: " + element);
+            }
+
+            socket.close();
+        } catch (EOFException e) {
+            System.out.println("Conexiunea a fost închisă prematur.");
+        } catch (SocketException e) {
+            System.out.println("Eroare de socket: " + e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            System.out.println("Eroare la conversia numerelor: " + e.getMessage());
+        }
+    }
+}
